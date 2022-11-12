eb91f49b7ec4145f538362d252c8b726442f199a
John Oblenda <joandong2@gmail.com>
2022-11-11 21:39:07 -0500

    wokring on the shop filter

--- header.php
+++ header.php
@@ -26,36 +26,45 @@
 	<a class="skip-link screen-reader-text" href="#primary"><?php esc_html_e( 'Skip to content', 'shoppi' ); ?></a>
 
 	<header id="masthead" class="site-header">
-		<div class="container">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-			if ( is_front_page() && is_home() ) :
-				?>
-				<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+		<div class="header container">
+			<div class="site-branding">
 				<?php
-			else :
-				?>
-				<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
-				<?php
-			endif;
-			$shoppi_description = get_bloginfo( 'description', 'display' );
-			if ( $shoppi_description || is_customize_preview() ) :
-				?>
-				<p class="site-description"><?php echo $shoppi_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></p>
-			<?php endif; ?>
-		</div><!-- .site-branding -->
+				the_custom_logo();
+				if ( is_front_page() && is_home() ) :
+					?>
+					<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
+					<?php
+				else :
+					?>
+					<p class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></p>
+					<?php
+				endif;
+				//$shoppi_description = get_bloginfo( 'description', 'display' );
+				//if ( $shoppi_description || is_customize_preview() ) :
+					?>
+					<!--p class="site-description"><?php //echo $shoppi_description; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?></!--p>
+				<?php //endif; ?>
+			</div><!-- .site-branding -->
 
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'shoppi' ); ?></button>
-			<?php
-			wp_nav_menu(
-				array(
-					'theme_location' => 'menu-1',
-					'menu_id'        => 'primary-menu',
-				)
-			);
-			?>
-		</nav><!-- #site-navigation -->
+			<nav id="site-navigation" class="main-navigation">
+				<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'shoppi' ); ?></button>
+				<?php
+				wp_nav_menu(
+					array(
+						'theme_location' => 'menu-1',
+						'menu_id'        => 'primary-menu',
+					)
+				);
+				?>
+			</nav><!-- #site-navigation -->
+			<div id="site-navigation" class="main-navigation">
+				shop icons
+			</div><!-- #site-navigation -->
 		</div>
 	</header><!-- #masthead -->
+
+	<div class="slider">
+		<?php if(is_front_page()) {
+			echo do_shortcode('[smartslider3 slider="2"]');
+		} ?>
+	</div>
